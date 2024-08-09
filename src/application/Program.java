d183845e494b6e194b9eaf1e67212848fcfdbabf
fdoargolo <felipeargolo75@gmail.com>
2024-08-08 23:55:07 -0300

    Reusing Insatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
@@ -7,10 +9,16 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(1);
+		
+		System.out.println("---TEST 1: seller findById----");
+		System.out.print("Enter the Id: ");
+		int id = scanner.nextInt();
+		
+		Seller seller = sellerDao.findById(id);
 		System.out.println(seller);
+		scanner.close();
 	}
 
 }
